0123a1a847671636185b64557fa0fff5e3b41eac
hongfu ruan <hongfu.ruan@intel.com>
2020-10-31 16:56:03 +0800

    seperate code to demo and TinySDK
    move logic to tinysdk and try to keep demo clean
    stream uri is passed from demo to sdk
    
    Signed-off-by: hongfu ruan <hongfu.ruan@intel.com>

--- TinySDK/src/main/java/com/example/tinysdk/ExoPlayerWrapper.java
+++ TinySDK/src/main/java/com/example/tinysdk/ExoPlayerWrapper.java
@@ -1,12 +1,150 @@
 package com.example.tinysdk;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.PlaybackPreparer;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MediaSourceFactory;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.StyledPlayerControlView;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultAllocator;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.EventLogger;
+import com.google.android.exoplayer2.util.Log;
+import com.google.android.exoplayer2.util.Util;
 
-public class ExoPlayerWrapper {
+import java.util.List;
+
+public class ExoPlayerWrapper implements
+        View.OnClickListener,
+        View.OnTouchListener,
+        Player.EventListener,
+        StyledPlayerControlView.VisibilityListener,
+        PlaybackPreparer {
+    private static final String TAG = "exo-wrapper";
+    private DataSource.Factory dataSourceFactory;
     private SimpleExoPlayer player;
-    protected ExoPlayerWrapper(Context context) {
-        player = new SimpleExoPlayer.Builder(context).build();
+    private List<MediaItem> mediaItems;
+    private DefaultTrackSelector trackSelector;
+    private DefaultTrackSelector.Parameters trackSelectorParameters;
+    private DefaultLoadControl mLoadControl;
+    private MediaSource mMediaSource;
+    private Context mContext;
+    private StyledPlayerView mPlayview;
+
+    public ExoPlayerWrapper(Context context) {
+        mContext = context;
+    }
+
+    public void onCreate(Bundle savedInstanceState, StyledPlayerView playerView) {
+        mPlayview = playerView;
+        mPlayview.setOnTouchListener(this);
+        mPlayview.requestFocus();
+
+        dataSourceFactory = new DefaultDataSourceFactory(mContext,
+                Util.getUserAgent(mContext, null));
+        DefaultTrackSelector.ParametersBuilder builder =
+                new DefaultTrackSelector.ParametersBuilder(/* context= */ mContext);
+        trackSelectorParameters = builder.build();
+
+        InitializePlayer();
+    }
+
+    private void InitializePlayer() {
+        Log.e(TAG, "InitializePlayer");
+        dataSourceFactory = new DefaultDataSourceFactory(mContext,
+                Util.getUserAgent(mContext, null), new DefaultBandwidthMeter.Builder(mContext).build());
+
+        DefaultLoadControl.Builder builder = new DefaultLoadControl.Builder();
+        builder.setAllocator(new DefaultAllocator(true, 2 * 1024 * 1024));
+        builder.setBufferDurationsMs(5000, 5000, 5000, 5000);
+        builder.setPrioritizeTimeOverSizeThresholds(true);
+        mLoadControl = builder.build();
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(mContext);
+        MediaSourceFactory mediaSourceFactory =
+                new DefaultMediaSourceFactory(dataSourceFactory);
+
+        player = new SimpleExoPlayer.Builder(mContext, renderersFactory)
+                .setMediaSourceFactory(mediaSourceFactory)
+                .build();
+        player.addAnalyticsListener(new EventLogger(trackSelector));
+        player.setAudioAttributes(AudioAttributes.DEFAULT, /* handleAudioFocus= */ true);
+        player.setPlayWhenReady(true);
+
+        mPlayview.setPlayer(player);
+        mPlayview.setPlaybackPreparer(this);
+    }
+
+    public void playStreambyUri(Uri stream_uri) {
+        Log.e(TAG, "playStreambyUri" + stream_uri.toString());
+        MediaSource[] mediaSources = new MediaSource[1];
+        mediaSources[0] = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(stream_uri));
+        mMediaSource = new ConcatenatingMediaSource(mediaSources);
+        player.setMediaSource(mMediaSource);
+        player.prepare();
+        return;
+    }
+
+
+    @Override
+    public void onVisibilityChange(int visibility) {
+        Log.e(TAG, "onVisibilityChange");
+    }
+
+    @Override
+    public void preparePlayback() {
+        Log.e(TAG, "preparePlayback");
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        Log.e(TAG, "onTouch");
+
+        FrameLayout layout = mPlayview.getOverlayFrameLayout();
+        return layout != null && view.getId() == layout.getId();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.e(TAG, "onClick");
+        Uri selected_uri;
+        switch (view.getId()) {
+            default:
+                break;
+        }
+    }
+
+    public void onStart() {
+    }
+
+    public void onResume() {
+    }
+
+    public void onPause() {
+    }
+
+    public void onStop() {
+    }
+
+    public void onDestroy() {
     }
 }
--- app/src/main/java/com/example/codechallenge/TinySDK_Demo.java
+++ app/src/main/java/com/example/codechallenge/TinySDK_Demo.java
@@ -2,53 +2,23 @@ package com.example.codechallenge;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.PlaybackPreparer;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.RenderersFactory;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.audio.AudioAttributes;
-import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.MediaSourceFactory;
-import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.ui.StyledPlayerControlView;
+import com.example.tinysdk.ExoPlayerWrapper;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultAllocator;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.EventLogger;
 import com.google.android.exoplayer2.util.Log;
-import com.google.android.exoplayer2.util.Util;
-
-import java.util.List;
 
 public class TinySDK_Demo extends AppCompatActivity
-        implements View.OnClickListener, StyledPlayerControlView.VisibilityListener, PlaybackPreparer, View.OnTouchListener, Player.EventListener {
+        implements View.OnClickListener{
 
     private static final String TAG = "demo-main";
+    protected ExoPlayerWrapper mWrapper;
 
     private StyledPlayerView playerView;
-    private DataSource.Factory dataSourceFactory;
-    private SimpleExoPlayer player;
-    private List<MediaItem> mediaItems;
-    private DefaultTrackSelector trackSelector;
-    private DefaultTrackSelector.Parameters trackSelectorParameters;
-    private DefaultLoadControl mLoadControl;
     private Uri[] VideosUris;
-    private MediaSource mMediaSource;
 
     public static final String test_stream1 = "https://demo.unified-streaming.com/video/smurfs/smurfs.ism/smurfs.mpd";
     public static final String test_stream2 = "https://media.axprod.net/TestVectors/v7-MultiDRM-SingleKey/Manifest_1080p.mpd";
@@ -58,6 +28,7 @@ public class TinySDK_Demo extends AppCompatActivity
     public void onCreate(Bundle savedInstanceState) {
         Log.e(TAG, "onCreate");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_tiny_s_d_k__demo);
         Button stream1_button = findViewById(R.id.test_stream_button1);
         Button stream2_button = findViewById(R.id.test_stream_button2);
@@ -65,99 +36,66 @@ public class TinySDK_Demo extends AppCompatActivity
         stream1_button.setOnClickListener(this);
         stream2_button.setOnClickListener(this);
         stream3_button.setOnClickListener(this);
-        findViewById(R.id.player_view).setOnTouchListener(this);
-
         playerView = findViewById(R.id.player_view);
-        playerView.setControllerVisibilityListener(this);
-        playerView.requestFocus();
-
-        dataSourceFactory = new DefaultDataSourceFactory(this.getBaseContext(),
-                Util.getUserAgent(this.getBaseContext(), this.getBaseContext().getString(R.string.app_name)), null);
-        DefaultTrackSelector.ParametersBuilder builder =
-                new DefaultTrackSelector.ParametersBuilder(/* context= */ this);
-        trackSelectorParameters = builder.build();
-
-        InitializePlayer();
-    }
-
-    private void InitializePlayer() {
-        Log.e(TAG, "InitializePlayer");
-        dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, this.getString(R.string.app_name)), new DefaultBandwidthMeter.Builder(this).build());
-
-        DefaultLoadControl.Builder builder = new DefaultLoadControl.Builder();
-        builder.setAllocator(new DefaultAllocator(true, 2 * 1024 * 1024));
-        builder.setBufferDurationsMs(5000, 5000, 5000, 5000);
-        builder.setPrioritizeTimeOverSizeThresholds(true);
-        mLoadControl = builder.build();
-
-        VideosUris = new Uri[3];
-        VideosUris[0] = Uri.parse(test_stream1);
-        VideosUris[1] = Uri.parse(test_stream2);
-        VideosUris[2] = Uri.parse(test_stream3);
-
-        RenderersFactory renderersFactory = new DefaultRenderersFactory(this.getApplicationContext());
-        MediaSourceFactory mediaSourceFactory =
-                new DefaultMediaSourceFactory(dataSourceFactory);
-
-        player = new SimpleExoPlayer.Builder(this, renderersFactory)
-                    .setMediaSourceFactory(mediaSourceFactory)
-                    .build();
-        player.addAnalyticsListener(new EventLogger(trackSelector));
-        player.setAudioAttributes(AudioAttributes.DEFAULT, /* handleAudioFocus= */ true);
-        player.setPlayWhenReady(true);
-
-        playerView.setPlayer(player);
-        playerView.setPlaybackPreparer(this);
-
-        MediaSource[] mediaSources = new MediaSource[3];
-        mediaSources[0] = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(VideosUris[0]));
-        mediaSources[1] = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(VideosUris[1]));
-        mediaSources[2] = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(VideosUris[2]));
-        mMediaSource = new ConcatenatingMediaSource(mediaSources);
-        player.setMediaSource(mMediaSource);
-        player.prepare();
+        mWrapper = new ExoPlayerWrapper(this);
+        mWrapper.onCreate(savedInstanceState, playerView);
     }
 
     @Override
     public void onStart() {
         Log.e(TAG, "onStart");
         super.onStart();
+        mWrapper.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        mWrapper.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        super.onPause();
+        mWrapper.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+        mWrapper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        mWrapper.onPause();
     }
 
     @Override
     public void onClick(View view) {
         Log.e(TAG, "onClick");
-
+        Uri selected_uri=null;
         switch (view.getId()) {
             case R.id.test_stream_button1:
                 Log.e(TAG, "to play stream #1");
+                selected_uri = Uri.parse(test_stream1);
                 break;
             case R.id.test_stream_button2:
                 Log.e(TAG, "to play stream #2");
+                selected_uri = Uri.parse(test_stream2);
                 break;
             case R.id.test_stream_button3:
                 Log.e(TAG, "to play stream #3");
+                selected_uri = Uri.parse(test_stream3);
                 break;
         }
+        if(selected_uri != null)
+            mWrapper.playStreambyUri(selected_uri);
     }
 
-    @Override
-    public void onVisibilityChange(int visibility) {
-        Log.e(TAG, "onVisibilityChange");
-    }
-
-    @Override
-    public void preparePlayback() {
-        Log.e(TAG, "preparePlayback");
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        Log.e(TAG, "onTouch");
-
-        FrameLayout layout = playerView.getOverlayFrameLayout();
-        return layout != null && view.getId() == layout.getId();
-
-    }
 }
